904b14403ea77181e725cfdf616dc333a9eea171
Dave Syer <dsyer@vmware.com>
2022-01-27 08:23:14 +0000

    Ugly async module like emcc generated

--- month.js
+++ month.js
@@ -1,3 +1,4 @@
-import monthFromDate from './months.js';
+const months = await import('./months.js');
+const monthFromDate = (await months.default()).monthFromDate;
 const dateString = process.argv[2] ?? null;
 console.log(monthFromDate(dateString));
--- months.js
+++ months.js
@@ -1,13 +1,38 @@
-const MONTHS = ['January', 'February', 'March','April', 'May', 'June', 
-  'July', 'August', 'September', 'October', 'November', 'December'];
-let monthFromDate = function (date) {
-  if (!date) {
-    date = null;
-  }
-  if (!(date instanceof Date)) {
-    date = new Date(date);
-  }
-  return MONTHS[date.getMonth()];
-}
-export { monthFromDate as default, monthFromDate };
-console.log(monthFromDate())
+var Module = (function () {
+
+  return (
+    function (Module) {
+      Module = Module || {};
+
+      var Module = typeof Module !== "undefined" ? Module : {};
+      var readyPromiseResolve, readyPromiseReject;
+      Module["ready"] = new Promise(function (resolve, reject) {
+        readyPromiseResolve = resolve;
+        readyPromiseReject = reject
+      });
+
+      let init = function() {
+        console.log(monthFromDate());
+        readyPromiseResolve(Module);
+      }
+
+      const MONTHS = ['January', 'February', 'March','April', 'May', 'June', 
+        'July', 'August', 'September', 'October', 'November', 'December'];
+      let monthFromDate = function (date) {
+        if (!date) {
+          date = null;
+        }
+        if (!(date instanceof Date)) {
+          date = new Date(date);
+        }
+        return MONTHS[date.getMonth()];
+      }
+      Module["monthFromDate"] = monthFromDate;
+      init();
+      return Module.ready
+    }
+  );
+
+})();
+
+export default Module;
