40dac25a793574073ac5bd439a3c4f30335961d4
Dave Syer <dsyer@vmware.com>
2022-02-01 09:34:50 +0000

    Get to the memory layout

--- person.c
+++ person.c
@@ -2,10 +2,32 @@
 #include <stdlib.h>
 #include "person.pb-c.h"
 
-int main() {
+Person *juergen() {
 	Person *person = malloc(sizeof(Person));
+	person__init(person);
 	person->id = "54321";
 	person->name = "Juergen";
-	printf("%s %s\n", person->id, person->name);
+	return person;
+}
+
+int size(Person *person) {
+	return person__get_packed_size(person);
+}
+
+void print(Person *person) {
+	printf("addr=%lu bsize=%d psize=%lu nsize=%lu id=%s (%lu) name=%s (%lu)\n", 
+		(long) person, size(person), sizeof(person), sizeof(*person), person->id, person->id, person->name, person->name);
+}
+
+void pack(Person *person, uint8_t *out) {
+	person__pack(person, out);
+}
+
+Person *unpack(uint8_t *data, int len) {
+	return person__unpack(NULL, len, data);
+}
+
+int main() {
+	print(juergen());
 	return 0;
 }
